cdb750adf6e71075a5c3af21fb9468a8ad8986c1
Ankita Gupta <ankitagupta2309@yahoo.co.in>
2016-06-21 17:39:20 +0530

    Added Command line arguments

--- start.js
+++ start.js
@@ -1,9 +1,10 @@
 var proxy = require('./lib/pushKnot');
+var argv = require('yargs').argv;
 
+var port = argv.port || 9002;
+var domain = argv.domain || false;
 var options = {
     timeout: 10,
     debug: true
 }
-
-var port = 9002;
-proxy(port, options )
+proxy(port, options, domain )
