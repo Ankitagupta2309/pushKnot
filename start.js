0c0fc6ca36d449ba8e970329236cdb2f55db398b
Ankita Gupta <ankitagupta2309@yahoo.co.in>
2016-06-22 20:40:11 +0530

    Added debug statements in respond.js

--- start.js
+++ start.js
@@ -3,8 +3,5 @@ var argv = require('yargs').argv;
 
 var port = argv.port || 9002;
 var domain = argv.domain || false;
-var options = {
-    timeout: 10,
-    debug: true
-};
-proxy(port, options, domain);
+
+proxy(port, domain);
